43f017786b66630ccd04915d46ee53ec9c0151e2
AngelinaEiras <73119057+AngelinaEiras@users.noreply.github.com>
2020-11-11 16:48:08 +0000

    FASTA com ficheiro abrto e leitura deste

--- sequencinator/intake.py
+++ sequencinator/intake.py
@@ -1,6 +1,3 @@
-import re
-
-
 def ler_seq(FileHandle):
     '''
     s
@@ -15,11 +12,37 @@ def ler_seq(FileHandle):
 
 def ler_FASTA_seq(FileHandle):
     '''
-    s
+    Parser de um ficheiro no formato FASTA para uma String
+    Dado um ficheiro aberto, com uma posição de cursor, lê a próxima sequência escrita no ficheiro.
+    Retorna uma String com a sequência, ou vazia quando atingir o EOF.
     '''
     seq = ""
-    with open(FileHandle, 'r') as file:
-        for line in file:
+    maiores = 0
+    while(maiores<2):
+        cursor=FileHandle.tell()
+        linha=FileHandle.readline()
+        if(linha==""): break
+        if linha[0]!='>':
+            seq+=linha
+        else:
+            maiores+=1
+    FileHandle.seek(cursor)
+    return seq.replace("\n", "")
 
-            seq.append(line)
-    return seq
+# main
+
+file = open(r"D:\Universidade\Mestradooo\Algoritmos para Análise de Sequências Biológicas\MBINF-AASB_Projeto\sequencinator\protein.txt", mode='r', encoding='utf-8')
+todas_as_seq = []
+i=0
+while(True):
+    i+=1
+    input("Carregue no ENTER para ler uma sequência (interromper com Ctrl+C)")
+    seq=ler_FASTA_seq(file)
+    if seq!="":
+        todas_as_seq.append(seq)
+        print("Sequencia", i)
+        print(seq)
+    else: 
+        break
+file.close
+
